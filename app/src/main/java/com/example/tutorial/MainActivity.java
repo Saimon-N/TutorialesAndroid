c6b5ae9b2c26ff8520c60391d612509ff95f08f9
Nicolas <oscuro959@gmail.com>
2020-09-24 11:13:02 +0800

    Aplicando Botones con imagenes

--- app/src/main/java/com/example/tutorial/MainActivity.java
+++ app/src/main/java/com/example/tutorial/MainActivity.java
@@ -1,5 +1,5 @@
-//hacemos uso de un ListView
-//nos permite crear una lista de elementos desplazables
+//hacemos uso de un Botton de imagen
+//nos permite crear un botton con imagen
 package com.example.tutorial;
 
 import android.support.v7.app.AppCompatActivity;
@@ -12,32 +12,27 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tv1;
-    private ListView lv1;
 
-    private String nombre[]={"Samuel","Valentina","Jesus","Francisco","Santiago","Miguel"};
-    private String edades[]={"18","25","25","21","20","23"};
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        tv1=(TextView)findViewById(R.id.tv1);
-        lv1=(ListView)findViewById(R.id.lv1);
-
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,R.layout.list_item_modificaciones,nombre);
-        lv1.setAdapter(adapter);
-
-        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                tv1.setText("La edad de "+nombre[i]+ " es " +edades[i]+" anios" );
-            }
-        });
+    //metodo para los botones boton 1
+    public void Mensaje1(View view)
+    {
+        Toast.makeText(this,"La imagen 1",Toast.LENGTH_SHORT).show();
+    }
+    //metodo para los botones boton 2
+    public void Mensaje2(View view)
+    {
+        Toast.makeText(this,"La imagen 2",Toast.LENGTH_SHORT).show();
     }
 }
